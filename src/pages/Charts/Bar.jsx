f6e8e3cfa8c6df4c5b5f07af125561e3595710a7
MaximProger <maksimkoshman090@gmail.com>
2023-03-17 17:59:45 +0300

    добавил настройки темы и несколько графиков

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  BarSeries,
+} from "@syncfusion/ej2-react-charts";
+
+import { Header } from "../../components";
+import { barCustomSeries, barChartData } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
   return (
-    <div>Bar</div>
-  )
-}
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Bar" />
+      <div className="w-full">
+        <ChartComponent
+          id="bar-chart"
+          height="420px"
+          lineWidth={1}
+          dataSource={barChartData}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        >
+          <Inject services={[BarSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {barCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
 
-export default Bar
+export default Bar;
