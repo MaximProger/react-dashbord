3afdbc3fa7ec24de73b953fc9da20044a5c7fa89
MaximProger <maksimkoshman090@gmail.com>
2023-03-20 11:26:04 +0300

    перенес недостающие графики и компоненты шапки из референса

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -4,32 +4,42 @@ import {
   SeriesCollectionDirective,
   SeriesDirective,
   Inject,
-  DateTime,
   Legend,
-  BarSeries,
+  Category,
+  Tooltip,
+  ColumnSeries,
+  DataLabel,
 } from "@syncfusion/ej2-react-charts";
 
-import { Header } from "../../components";
-import { barCustomSeries, barChartData } from "../../data/dummy";
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { ChartsHeader } from "../../components";
 import { useStateContext } from "../../contexts/ContextProvider";
 
 const Bar = () => {
   const { currentMode } = useStateContext();
+
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Chart" title="Bar" />
-      <div className="w-full">
+      <ChartsHeader category="Bar" title="Olympic Medal Counts - RIO" />
+      <div className=" w-full">
         <ChartComponent
-          id="bar-chart"
-          height="420px"
-          lineWidth={1}
-          dataSource={barChartData}
+          id="charts"
+          primaryXAxis={barPrimaryXAxis}
+          primaryYAxis={barPrimaryYAxis}
           chartArea={{ border: { width: 0 } }}
           tooltip={{ enable: true }}
           background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
         >
-          <Inject services={[BarSeries, DateTime, Legend]} />
+          <Inject
+            services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+          />
           <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
             {barCustomSeries.map((item, index) => (
               <SeriesDirective key={index} {...item} />
             ))}
